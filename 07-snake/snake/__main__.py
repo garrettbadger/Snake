865c3b102d62bc023821ea71dcba3ae266a52e51
garrettbadger <garrett.badger.id@gmail.com>
2022-10-31 16:11:51 -0600

    Snake

---
+++ 07-snake/snake/__main__.py
@@ -0,0 +1,15 @@
+import os
+os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\flipp\cse-210-solo-checkpoints\cse210-student-solo-checkpoints\07-snake\raylib-2.0.0-Win64-mingw\raylib-2.0.0-Win64-mingw\lib'
+from game.director import Director
+from game.input_service import InputService
+from game.output_service import OutputService
+
+def main():
+    input_service = InputService()
+    output_service = OutputService()
+    director = Director(input_service, output_service)
+    director.start_game()
+
+if __name__ == "__main__":
+    main()
+
